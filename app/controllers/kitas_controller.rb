9a539e666dbc4e0105503f9a27218ffe8987b785
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-29 16:03:33 +0200

    created kitas controller and routes

---
+++ app/controllers/kitas_controller.rb
@@ -0,0 +1,2 @@
+class KitasController < ApplicationController
+end
---
+++ test/controllers/kitas_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class KitasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
