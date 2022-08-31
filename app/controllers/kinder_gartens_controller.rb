de2fc5a1ff30f420d5e657b14ada23812f90b9ce
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-31 12:58:39 +0200

    started implement map and geocoding

--- app/controllers/kinder_gartens_controller.rb
+++ app/controllers/kinder_gartens_controller.rb
@@ -1,11 +1,22 @@
 class KinderGartensController < ApplicationController
+  def display_markers
+    @markers = @kinder_gartens.geocoded.map do |k|
+      {
+        lat: k.latitude,
+        lng: k.longitude
+      }
+    end
+  end
+
   def index
     if params[:query].present?
       sql_query = "address ILIKE :query"
       @kinder_gartens = KinderGarten.where(sql_query, query: "%#{params[:query]}%")
+      # @kinder_gartens = KinderGarten.near(sql_query, query: "%#{params[:query]}%", 5)
     else
       @kinder_gartens = KinderGarten.all
     end
+    display_markers
   end
 
   def show
