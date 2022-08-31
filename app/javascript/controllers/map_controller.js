8d4f90b6e7d5c50b197ed21bc4014c4ef9e86c82
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-31 14:54:37 +0200

    display map ok

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="map"
 export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
   connect() {
+    console.log('Hello from Stimulus')
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [13.404954, 52.520007], // [lng, lat]
+      zoom: 9,
+      style: 'mapbox://styles/lamoureuxmarie/cl7hkl5lc002s15s02nlingxr'
+    })
   }
 }
