327473e0493e3fa70ea4115acdc78397f0a3a289
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-09-04 09:49:37 +0200

    #reminder_mailer

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,9 +1,12 @@
 class ReminderMailer < ApplicationMailer
   default from: 'contact@my-kita.xyz'
 
-  def reminder_email
+  def reminder_mailer
     @user = params[:user]
+    @reminder = params[:reminder]
     @url = 'https://www.my-kita.xyz/reminders'
-    mail(to: @user.email, subject: 'Reminder')
+    mail(from: "Marie <contact@my-kita.xyz>",
+         to: User.last.email,
+         subject: 'Reminder')
   end
 end
--- test/mailers/previews/reminder_mailer_preview.rb
+++ test/mailers/previews/reminder_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/reminder_mailer
 class ReminderMailerPreview < ActionMailer::Preview
-
+  def reminder_mailer
+    ReminderMailer.with(user: User.last, reminder: Reminder.last).reminder_mailer
+  end
 end
