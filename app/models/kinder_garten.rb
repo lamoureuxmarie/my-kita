cfa2cf371808e3dcbe365596e083bc0b181777b4
rubensy <86684948+rubensy@users.noreply.github.com>
2022-09-05 10:29:33 +0200

    added borough options to dropdown search

--- app/models/kinder_garten.rb
+++ app/models/kinder_garten.rb
@@ -3,4 +3,17 @@ class KinderGarten < ApplicationRecord
   validates :name, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  BOROUGH = ['Charlottenburg-Wilmersdorf',
+             'Friedrichshain-Kreuzberg',
+             'Lichtenberg',
+             'Marzahn-Hellersdorf',
+             'Mitte',
+             'Neukölln',
+             'Pankow',
+             'Reinickendorf',
+             'Spandau',
+             'Steglitz-Zehlendorf',
+             'Tempelhof-Schöneberg',
+             'Treptow-Köpenick']
 end
