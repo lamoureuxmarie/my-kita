04b5e216b11687b55776a8d72ce9afd6d52784c1
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-09-06 17:39:31 +0200

    fixed alert url

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2'
 export default class extends Controller {
   static values = { redirectUrl: String}
   connect() {
-    console.log('Controller connected')
+    console.log('Controller connected', this.redirectUrlValue)
   }
 
   showAlert() {
@@ -18,7 +18,7 @@ export default class extends Controller {
       title: 'Added Successfully!',
       showConfirmButton: true,
       showDenyButton: true,
-      confirmButtonText: `<a href="${this.redirectUrl}">See list</a>`,
+      confirmButtonText: `<a href="${this.redirectUrlValue}">See list</a>`,
       confirmButtonColor: '#FFE23F',
       denyButtonText: 'Go Back',
       denyButtonColor: '#F5F1E9'
