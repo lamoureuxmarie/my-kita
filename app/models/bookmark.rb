ceec14971f9f5fd9a05288b5727a0dcb836e4254
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-09-06 21:42:36 +0200

    need to fix pundit before going further

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,14 +2,11 @@ class Bookmark < ApplicationRecord
   belongs_to :kinder_garten
   belongs_to :user
   has_many :reminders
-<<<<<<< HEAD
   has_many :templates
-=======
 
   # ICONS = {
   #   "Send Email" => "fa-solid fa-envelope icon-status";
   #   "Make a call" => "fa-solid fa-phone icon-status";
 
   # }
->>>>>>> aa525cba5ea31be13613c073548adee22103c47e
 end
